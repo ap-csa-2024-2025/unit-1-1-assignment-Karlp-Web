ff94528b73b8c7ecbd51b9080a89ea5797321fd3
Karlp-Web <karlenportillo1>
2024-09-03 11:24:09 -0700

    yomomma

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,22 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello world!");
+    System.out.println(x:"Hello world!");
+    system.out.println(x:"Hello world!");
+
+    int myage;
+    myage =999;
+
+    double price = 4.99
+    String myname = "Karlen Port";
+
+    int yomomma;
+    yomomma = 1;
+
+    double money = 5.99;
+
+    String bankBalance ="Broke";
+
+    boolean moneyUpnotFunnyUp = false;
   }
 }
